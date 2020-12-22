c619dfd436f57b54ce0284f35466250849da0fd6
Michael <code@mweiss.ch>
2020-12-22 08:54:05 +0100

    Test experimental methods availability

---
+++ tests_unit/keras_assumptions_tests/__init__.py
@@ -0,0 +1,4 @@
+"""Tests in this package do *not* actually test uncertainty-wizard,
+but they make sure that assumptions about tensorflow which have been taken
+and tested during uncertainty-wizard, but which are not yet stable or well documented,
+are still true in any further tf version for which the tests are executed."""
