e87cb1db6bf4991d4bcb26cf1cb512aee6f4570b
Michael <code@mweiss.ch>
2020-12-22 08:57:55 +0100

    Format

--- tests_unit/keras_assumptions_tests/test_experimental_calls_exist.py
+++ tests_unit/keras_assumptions_tests/test_experimental_calls_exist.py
@@ -15,27 +15,37 @@ class TestExperimentalAPIAreAvailable(TestCase):
 
     def test_list_physical_devices(self):
         self.assertTrue("list_physical_devices" in dir(tf.config.experimental))
-        parameters = inspect.signature(tf.config.experimental.list_physical_devices).parameters
-        self.assertTrue('device_type' in parameters)
+        parameters = inspect.signature(
+            tf.config.experimental.list_physical_devices
+        ).parameters
+        self.assertTrue("device_type" in parameters)
         self.assertEqual(1, len(parameters))
 
     def test_virtual_device_configuration(self):
         self.assertTrue("VirtualDeviceConfiguration" in dir(tf.config.experimental))
-        parameters = inspect.signature(tf.config.experimental.VirtualDeviceConfiguration).parameters
-        self.assertTrue('memory_limit' in parameters)
-        self.assertTrue('experimental_priority' in parameters)
+        parameters = inspect.signature(
+            tf.config.experimental.VirtualDeviceConfiguration
+        ).parameters
+        self.assertTrue("memory_limit" in parameters)
+        self.assertTrue("experimental_priority" in parameters)
         self.assertEqual(2, len(parameters))
 
     def test_set_visible_devices(self):
         self.assertTrue("set_visible_devices" in dir(tf.config.experimental))
-        parameters = inspect.signature(tf.config.experimental.set_visible_devices).parameters
-        self.assertTrue('devices' in parameters)
-        self.assertTrue('device_type' in parameters)
+        parameters = inspect.signature(
+            tf.config.experimental.set_visible_devices
+        ).parameters
+        self.assertTrue("devices" in parameters)
+        self.assertTrue("device_type" in parameters)
         self.assertEqual(2, len(parameters))
 
     def test_set_virtual_device_configuration(self):
-        self.assertTrue("set_virtual_device_configuration" in dir(tf.config.experimental))
-        parameters = inspect.signature(tf.config.experimental.set_virtual_device_configuration).parameters
-        self.assertTrue('device' in parameters)
-        self.assertTrue('logical_devices' in parameters)
+        self.assertTrue(
+            "set_virtual_device_configuration" in dir(tf.config.experimental)
+        )
+        parameters = inspect.signature(
+            tf.config.experimental.set_virtual_device_configuration
+        ).parameters
+        self.assertTrue("device" in parameters)
+        self.assertTrue("logical_devices" in parameters)
         self.assertEqual(2, len(parameters))
